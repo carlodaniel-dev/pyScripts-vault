965255953ba73169c9162d7b00c21d9029277403
Carlos Salcán <carlosdanielsm.dev@gmail.com>
2026-03-23 17:08:57 -0500

    Add WhatsApp message automation script

---
+++ whatsapp-auto-sender/main.py
@@ -0,0 +1,7 @@
+from pywinauto import Application
+import time
+
+# 1. Configuración de datos
+contacto = input('Nombre del contacto/grupo: ')
+mensaje = input('Mensaje a enviar: ')
+cantidad = int(input('¿Cuántas veces?: '))
